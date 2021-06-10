15bfd909514e688ee95bd69a60f8114db28ce293
starrobe <starrobe@163.com>
2021-06-10 11:10:13 +0000

    Update Hexo Static Content

--- js/load-aplayer.js
+++ js/load-aplayer.js
@@ -16,25 +16,29 @@ document.addEventListener("DOMContentLoaded", function() {
       },
       {
         name: '遥远的她',
-        artist: 'https://cdn.jsdelivr.net/gh/starrobe/cdn/audio/遥远的她 - 张学友.mp3',
+        artist: '张学友',
+        url: 'https://cdn.jsdelivr.net/gh/starrobe/cdn/audio/遥远的她 - 张学友.mp3',
         cover: 'https://cdn.jsdelivr.net/gh/starrobe/cdn/img/music_pic/zxy-0.jpg',
         lrc: 'https://cdn.jsdelivr.net/gh/starrobe/cdn/lrc/遥远的她 - 张学友.lrc'
       },
       {
         name: '月半小夜曲',
-        artist: 'https://cdn.jsdelivr.net/gh/starrobe/cdn/audio/月半小夜曲 - 李克勤.mp3',
+        artist: '李克勤',
+        url: 'https://cdn.jsdelivr.net/gh/starrobe/cdn/audio/月半小夜曲 - 李克勤.mp3',
         cover: 'https://cdn.jsdelivr.net/gh/starrobe/cdn/img/music_pic/lkq-0.jpg',
         lrc: 'https://cdn.jsdelivr.net/gh/starrobe/cdn/lrc/月半小夜曲 - 李克勤.lrc'
       },
       {
         name: '天使的翅膀',
-        artist: 'https://cdn.jsdelivr.net/gh/starrobe/cdn/audio/天使的翅膀 - 徐誉滕.mp3',
+        artist: '徐誉滕',
+        url: 'https://cdn.jsdelivr.net/gh/starrobe/cdn/audio/天使的翅膀 - 徐誉滕.mp3',
         cover: 'https://cdn.jsdelivr.net/gh/starrobe/cdn/img/music_pic/xyt-0.jpg',
         lrc: 'https://cdn.jsdelivr.net/gh/starrobe/cdn/lrc/天使的翅膀 - 徐誉滕.lrc'
       },
       {
         name: '一生所爱',
-        artist: 'https://cdn.jsdelivr.net/gh/starrobe/cdn/audio/一生所爱 - 卢冠廷.mp3',
+        artist: '卢冠延',
+        url: 'https://cdn.jsdelivr.net/gh/starrobe/cdn/audio/一生所爱 - 卢冠廷.mp3',
         cover: 'https://cdn.jsdelivr.net/gh/starrobe/cdn/img/music_pic/lgy-0.jpg',
         lrc: 'https://cdn.jsdelivr.net/gh/starrobe/cdn/lrc/一生所爱 - 卢冠廷.lrc'
       }     
